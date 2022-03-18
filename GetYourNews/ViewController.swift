d073c95c9badf9445ec48ab77e178d754b05990e
ajandaur <anmol.jandaur@gmail.com>
2022-03-18 11:56:47 -0700

    Initial Commit

---
+++ GetYourNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GetYourNews
+//
+//  Created by Anmol  Jandaur on 3/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
